e18fa0e43d11dcfb4322d85450618c55b199c951
Nico Rithner <nicorithner@gmail.com>
2020-09-21 09:47:04 -0600

    Upgrade flight model to have many passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
   validates :number, :date, :time, :departure_city, :arrival_city, presence: true
   belongs_to :airline
+  has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
 end
